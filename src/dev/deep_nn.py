4017bf4ab034da9035703f5354355506e41fbac2
Bruno Santome <bruno.santomeantolin@gmail.com>
2025-11-16 20:57:11 +0000

    Forward pass implementation

--- src/dev/deep_nn.py
+++ src/dev/deep_nn.py
@@ -1,4 +1,12 @@
 import numpy as np
+from dnn_functions import (
+    sigmoid,
+    relu,
+    softmax,
+    sigmoid_back_pass,
+    relu_back_pass,
+    softmax_back_pass,
+)
 
 
 class NeuronalNetwork:
@@ -11,12 +19,12 @@ class NeuronalNetwork:
         learning_rate=0.01,
         seed=42,
         hidden_activation="relu",
-        output_activation="softmax",
+        output_activation="sigmoid",
         dropout=False,
         batch_size=1,
     ):
-        self.layers_units = layers_units
-        self.layers = len(layers_units)
+        self.num_layers_units = layers_units
+        self.num_layers = len(layers_units)
         self.learning_rate = learning_rate
         self.epoch = epoch
         self.seed = seed
@@ -24,6 +32,8 @@ class NeuronalNetwork:
         self.output_activation = output_activation
         self.dropout = dropout
         self.batch_size = batch_size
+        self.param = {}
+        self.storage_layers = []
         # self.init_param() only called when the data is fitted into the model (to fit dimension of first input layer)
 
     def init_param(self):
@@ -34,24 +44,59 @@ class NeuronalNetwork:
         """
         # example of layers: [4,8,4,1]
         # Weights: (4,8), (8,4), (4,1), (4,1)
-        param = {}
-        np.random.seed = self.seed
-        for l in range(1, self.layers):
+        np.random.seed(self.seed)
+        for l in range(1, self.num_layers):
             # Dimensions of Wl: (n[l], n[l-1]) ==> it is the same for dWl (for backward pass)
-            param[f"W{l}"] = np.random.rand(
-                self.layers_units[l], self.layers_units[l - 1]
+            self.param[f"W{l}"] = np.random.rand(
+                self.num_layers_units[l], self.num_layers_units[l - 1]
             )
             # Dimensions of bl: (n[l], 1) ==> it is the same for dbl (for backward pass)
-            param[f"b{l}"] = np.zeros((self.layers_units[l], 1))
+            self.param[f"b{l}"] = np.zeros((self.num_layers_units[l], 1))
 
-        return param
+    def forward_pass_hidden_single(self, activation_last, W, b):
+        Z = np.dot(W, activation_last) + b
+        if self.hidden_activation == "relu":
+            activation_current, Z = relu(Z)
+        if self.hidden_activation == "sigmoid":  # We could implement tanh
+            # It might not be useful to return the Z value too.
+            activation_current, Z = sigmoid(Z)
+        return activation_current, (Z, W, b, activation_last)
 
-    def forward_pass(self):
-        ...
+    def forward_pass_output_layer(self, activation_last, W, b):
+        print(W.shape)
+        print(activation_last)
+        Z = np.dot(W, activation_last) + b
+        if self.output_activation == "softmax":
+            activation_current, Z = softmax(Z)
+        if self.output_activation == "sigmoid":
+            activation_current, Z = sigmoid(Z)
+        return activation_current, (Z, W, b, activation_last)
+
+    def forward_pass(self, X):
         # input: a[l-1]
         # z[l] = W[l]*A[l-1] + b[l]
         # a[l] = g[l](z[l])
         # output: a[l], storage: (z[l]: (W[l]*a[l-1]+b[l]), W[l], b[l], a[l-1])
+
+        activation_current = X
+        layers = self.num_layers - 1  # -1 because the first layer does not count.
+        # Hidden layers
+        for l in range(1, layers):
+            activation_last = activation_current
+            activation_current, storage = self.forward_pass_hidden_single(
+                activation_last, self.param[f"W{l}"], self.param[f"b{l}"]
+            )
+            self.storage_layers.append(storage)
+
+        # Output layer
+        activation_output, storage = self.forward_pass_output_layer(
+            activation_current,
+            self.param[f"W{layers}"],
+            self.param[f"b{layers}"],
+        )
+        self.storage_layers.append(storage)
+
+        return activation_output
 
     def loss_calc(self):
         ...
--- src/dev/dnn_tests.py
+++ src/dev/dnn_tests.py
@@ -1,19 +1,37 @@
 import numpy as np
 import unittest
 from deep_nn import NeuronalNetwork
+from dnn_functions import (
+    sigmoid,
+    relu,
+    softmax,
+    sigmoid_back_pass,
+    relu_back_pass,
+    softmax_back_pass,
+)
 
 
 class dnn_tests(unittest.TestCase):
     def test_init_param(self):
         # [input, hiddeen1, ..., hiddenN, output]
         # todo: important make a method to input data and transform that data into the first input layer
-        nn_architecture1 = [2, 4, 1]
+        nn_architecture1 = [2, 4, 4, 1]
         nn_architecture2 = [8, 4, 2, 1]
         NNTest = NeuronalNetwork(nn_architecture1)
         parameters = NNTest.init_param()
-        print(parameters)
+
+    def test_forward_pass(self):
+        np.random.seed(42)
+        num_values = 2
+        X = np.random.randn(num_values, 3)
+        # print(X)
+        nn_architecture1 = [num_values, 4, 4, 5, 1]
+        NNTest2 = NeuronalNetwork(nn_architecture1, seed=42)
+        NNTest2.init_param()
+        print(NNTest2.forward_pass(X))
 
 
 if __name__ == "__main__":
     Tests = dnn_tests()
-    Tests.test_init_param()
+    # Tests.test_init_param()
+    Tests.test_forward_pass()
