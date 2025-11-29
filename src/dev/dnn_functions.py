Softmax numerical instability fixed, wrong axis sum in softmax. Categorical Cross entropy implementation

src/dev/dnn_functions.py
@@ -86,7 +86,9 @@ def softmax(Z):
     Numerical issues on tests, when calculating the loss it explodes "np.exp(Z)" this becomes very large or very small 
     When I then do the log() it produces a nan value, a very high or small number
     """
-    A = np.exp(Z) / np.sum(np.exp(Z))
+    Zmax = np.max(Z, axis=1, keepdims=True)
+    A = np.exp(Z - Zmax) / np.sum(np.exp(Z - Zmax), axis=1, keepdims=True)
+
     return A, Z
 
 