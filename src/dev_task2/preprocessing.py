c2a96ab7c0c9da244527d9d4f8b8eed428882bce
Bruno Santome <bruno.santomeantolin@gmail.com>
2025-12-20 18:13:19 +0000

    Dataset preprocessing Dataloader. Ready to input NN

---
+++ src/dev_task2/preprocessing.py
@@ -0,0 +1,70 @@
+import torch
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+
+"""
+Details 
+
+Aerial Landscape Images 
+
+(161.48 MB)
+
+Image Resolution: 256x256 pixels
+Number of categories: 15
+Categories: 
+Agriculture, Airport, Beach, City, Desert, 
+Forest, Grassland,Highway, Lake, Mountain, 
+Parking, Port, Railway, Residential, River
+
+Number of Images per Category: 800
+
+Sources: 
+AID Dataset: https://captain-whu.github.io/AID/
+
+NWPU-Resisc45 Dataset: https://paperswithcode.com/dataset/resisc45
+
+"""
+
+PATH = "C:/Users/bruno/OneDrive/Escritorio/Desktop/Repositories/Programming_math_Ai_Assessmment_Msc_Ai/dataset/Aerial_Landscapes/"
+
+
+def pre_processing_dataset(batch_size=32, train_test_split_n=0.2, seed=42):
+    # Careful if adding augmentation or transformations to the data better to do for training,
+    # so apply after the split not before.
+    transform = transforms.Compose(
+        [
+            # transforms.Resize((256, 256)), ## check if reducing the size is worth it.
+            # transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ]
+    )
+    landscapes = datasets.ImageFolder(root=PATH)
+    training_split_n = 1 - train_test_split_n
+    gen = torch.Generator().manual_seed(
+        seed
+    )  # generator to have a fixed seed when splitting. to recreate results.
+    train_size = int(training_split_n * len(landscapes))
+    train_set, test_set = random_split(
+        landscapes, [train_size, len(landscapes) - train_size], generator=gen
+    )
+    train_set.dataset.transform = transform
+    test_set.dataset.transform = transform
+    # DataLoader: Groups samples into batches,
+    # Shuffles data, loads data in parallel,
+    # Feeds data efficiently to the GPU
+    # The batching helps for a faster GPU computation and more stable gradients
+
+    landscapes_train = DataLoader(
+        train_set, batch_size=batch_size, shuffle=True, num_workers=2
+    )
+    landscapes_test = DataLoader(test_set, batch_size=batch_size, num_workers=2)
+
+    # Disk >  Dataset > Transform > DataLoader > Model
+
+    # for X, y in landscapes_test:
+    #     print(f"Shape of X : {X.shape}")
+    #     print(f"Shape of y: {y.shape} {y.dtype}")
+    #     break
+    return landscapes_train, landscapes_test
--- src/dev_task2/pytorch_nn_dev.py
+++ src/dev_task2/pytorch_nn_dev.py
@@ -1,22 +1,38 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import random_split
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, accuracy_score
 from torch.utils.data import DataLoader
 from sklearn.preprocessing import LabelEncoder
+from preprocessing import pre_processing_dataset
+from torchvision import datasets, transforms
 import sys
 
-print(sys.executable)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using {device}")
-print(torch.cuda.get_device_name(0))
 
-print("Torch version:", torch.__version__)
-print("CUDA available:", torch.cuda.is_available())
-print("CUDA version:", torch.version.cuda)
+def swtich_to_cuda():
+    print(sys.executable)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using {device}")
+    print(torch.cuda.get_device_name(0))
+
+    print("Torch version:", torch.__version__)
+    print("CUDA available:", torch.cuda.is_available())
+    print("CUDA version:", torch.version.cuda)
 
 
-class NeuronalNetwork(nn.Module):
-    def __init__(self):
-        super().__init__
+PATH = "C:/Users/bruno/OneDrive/Escritorio/Desktop/Repositories/Programming_math_Ai_Assessmment_Msc_Ai/dataset/Aerial_Landscapes/"
+
+
+# class NeuronalNetwork(nn.Module):
+#     def __init__(self):
+#         super().__init__
+
+
+if __name__ == "__main__":
+    X_train, y_train = pre_processing_dataset()
+    # for X, y in X_train:
+    #     print(f"Shape of X : {X.shape}")
+    #     print(f"Shape of y: {y.shape} {y.dtype}")
+    #     break
